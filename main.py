8abd8605421a81f3effd0bd37163dea4e395d757
xhimanshuz <hi.himanshu14@gmail.com>
2018-07-01 00:01:16 +0530

    AboutUs dialog window add with Source code documentation

--- main.py
+++ main.py
@@ -2,32 +2,29 @@
 
 
 # ========= TODO LIST ========= #
-# FORMAT BUTTON WORKING
-# FLASH BUTTON WORKING
-# AUTO DOWNLOAD AND SELECT OS
-# BOOTLOADER AUTO INSTALLER
-# LOG ENGINE (DISPLAY ERRORS AND MESSAGES)
-# PROGRESS DISPLAY
+# FORMAT BUTTON WORKING [X]
+# FLASH BUTTON WORKING  [X]
+# AUTO DOWNLOAD AND SELECT OS [X]
+# BOOTLOADER AUTO INSTALLER [X]
+# LOG ENGINE (DISPLAY ERRORS AND MESSAGES) [*]
+# PROGRESS DISPLAY  [*]
+#
+# [X] = DONE, [*] = IN DEVELOPMENT
 
 # ========= CONTRIBUTERS ========= #
 # Himanshu Rastogi <hi.himanshu14@gmail.com> --> AUTHOR
 # ...?
 
 
-
-from sys import argv
-import gi
+import gi                           # IMPORTING GTK MODULES
 gi.require_version('Gtk','3.0')
 from gi.repository import Gtk
-import subprocess
-import json
+import subprocess                   # CONTROL GNU/LINUX PROCESS
+import json                         # JSON API - TO FETCH JSON DATA
 
 class FlashyPi(Gtk.Window):
     def __init__(self):
-        self.dType = 'usb'
-        # if self.dType is None:
-        #     self.dType = 'usb'
-        #VARIABLE
+        self.dType = 'usb'          
         self.drive = '/dev/null'
         self.choice = ''
         self.bootloaderChoice = ''
@@ -64,13 +61,11 @@ class FlashyPi(Gtk.Window):
         # self.fileReloadButton.connect('clicked', self.fileReloadButtonClicked)
 
         self.driveComboBox = self.builder.get_object('driveComboBox')
-        self.listStoreCreate()
-        self.driveComboBox.set_model(self.listStore)
-        self.driveComboBox.pack_start(self.cellRenderer, 0)
-        self.driveComboBox.add_attribute(self.cellRenderer, 'text', 1)
-        self.addToListStore()
         self.driveComboBox.connect('changed', self.onChange)
+        self.listStoreCreate()
+        self.addToListStore()
         self.driveComboBox.set_active(0)
+        self.deviceReloadButton = self.builder.get_object('deviceReloadButton')
 
         self.formatButton = self.builder.get_object('formatButton')
         self.otherButton = self.builder.get_object('otherButton')
@@ -81,60 +76,86 @@ class FlashyPi(Gtk.Window):
         self.textBuffer = Gtk.TextBuffer()
         self.textView.set_buffer(self.textBuffer)
 
+        self.about = self.builder.get_object('about')
+        self.about.connect('response', self.aboutClose)
+        
         signal = {
-            'fileReloadButtonClicked':self.fileReloadButtonClicked,
-            'formatButtonClicked':self.formatButtonClicked,
-            'flashButtonClicked':self.flashButtonClicked,
-            'otherButtonClicked':self.otherButtonClicked
+            'fileReloadButtonClicked':  self.fileReloadButtonClicked,
+            'formatButtonClicked':      self.formatButtonClicked,
+            'flashButtonClicked':       self.flashButtonClicked,
+            'aboutButtonClicked':       self.aboutButtonClicked,
+            'deviceReloadButtonClicked':self.deviceReloadButtonClicked
         }
 
         self.builder.connect_signals(signal)
 
+    def aboutClose(self, action, param):        # CLOSING ABOUT DIALOG
+        action.hide()
 
-    def bootloaderToggled(self, widget, bootloader):
+    def aboutButtonClicked(self, widget,):      # CALLED WHEN ABOUT BUTTON CLICKED
+        self.about.run()
+        print(self.mountpoint())
+
+    def deviceReloadButtonClicked(self, widget):    # RELOAD CURRENT USB DRIVE LIST
+        self.addToListStore()
+
+    def bootloaderToggled(self, widget, bootloader):    # CALLED WHEN BOOTLOADER RADIO BUTTON SELECTED 
         self.bootloaderChoice = bootloader
         self.msg(self.bootloaderChoice, " Selected")
 
-    def onChoiceToggled(self, widget, choice):
+    def onChoiceToggled(self, widget, choice):      # CALLED WHEN FLASH METHOD RADIO BUTTON SELECTED 
         self.choice = choice
 
-    def listStoreCreate(self):
+    def listStoreCreate(self):                      # IS USED TO CREATE LIST OF AVAILABLE USB DRIVE
         self.listStore = Gtk.ListStore(str, str)
         self.cellRenderer = Gtk.CellRendererText()
+        self.driveComboBox.set_model(self.listStore)
+        self.driveComboBox.pack_start(self.cellRenderer, 0)
+        self.driveComboBox.add_attribute(self.cellRenderer, 'text', 1)
+        
 
-    def addToListStore(self):
+    def addToListStore(self):                       # IS USED TO ADD AVAILABLE USB DRIVE TO LIST 
         data = self.getFromJson()
+        self.listStore.clear()
         self.listStore.append(('null','--- Please Select Device ---'))
+        self.driveComboBox.set_active(0)
         for i in data:
             if i['tran'] == self.dType:
                 tuple = (i['name'], '/dev/' + str(i['name'] + " | " + i['model'] + " | " + i['size']))
                 print(tuple)
                 self.listStore.append(tuple)
 
-    def getFromJson(self):
+    def getFromJson(self):                         # FETCH JSON DATA FOR DRIVE DETAIL
         self.jsonData = json.loads(subprocess.getstatusoutput('lsblk -I 8 -n -J -O')[1])
         self.jsonData = self.jsonData['blockdevices']
         # print(jsonData)
         return self.jsonData
 
-    def fileReloadButtonClicked(self, widget):
+    def fileReloadButtonClicked(self, widget):      # EMPTY SELECTED FILE FROM FILE CHOOSER 
         self.fileChooer.set_filename('None')
 
-    def onChange(self, widget):
-        self.drive = '/dev/' + self.driveComboBox.get_model()[self.driveComboBox.get_active_iter()][0]  #SETTING DRIVE LOCATION eg: '/dev/sdb1'
+    def onChange(self, widget):                     # CALLED WHEN YOU SELECT USB DRIVE
+        if widget.get_active_iter() is not None:
+            self.drive = '/dev/' + self.driveComboBox.get_model()[self.driveComboBox.get_active_iter()][0]  #SETTING DRIVE LOCATION eg: '/dev/sdb1'
         print(self.drive,'mountpoint',self.mountpoint())
 
     def mountpoint(self):
         mp = " ".join(subprocess.getstatusoutput('lsblk -I 8 -l -n | grep {}'.format(self.drive.replace('/dev/','')+'1'))[1].split()[6:]) 
         return mp
 
+    # THIS FUCTION IS USED TO FORMAT SELECTED DRIVE
     def formatDrive(self):
         print('Formatting ',self.drive+'1')
-        flag = subprocess.getstatusoutput('sudo umount {}*'.format(self.drive))
+        flag = subprocess.getstatusoutput('sudo umount {}*'.format(self.drive)) # UNMOUNTING SELECTED DRIVE
         if flag[0]:
-            self.msg('Error in Unmounting\n', flag[1])
+            self.msg('Error in Unmounting\n', flag[1])  #ERROR IF THERE IS PROBLEM IN UNMOUNTING
         else:
             self.msg('Successfully Unmounted')
+        flag = subprocess.getstatusoutput('sudo fatlabel {} FlashyPi'.format(self.drive+'1'))   #
+        if flag[0]:
+            self.msg('Error in Renaming ', flag[1])
+        else:
+            self.msg('Successfully Renamed ', flag[1])
         flag = subprocess.getstatusoutput('sudo parted {} mklabel msdos mkpart primary fat32 1MiB 100% set 1 boot on -s'.format(self.drive))
         if flag[0]:
             print('Error in Partitioning: {}'.format(self.drive), flag[1])
@@ -153,7 +174,7 @@ class FlashyPi(Gtk.Window):
                 else:
                     return False
 
-    def mountDrive(self):
+    def mountDrive(self):                                   # MOUNTING THE SELECT DRIVE
         subprocess.getstatusoutput('sudo mkdir /media/usb')
         flag = subprocess.getstatusoutput('mount {} /media/usb'.format(self.drive+'1'))
         if flag[0]:
@@ -164,68 +185,9 @@ class FlashyPi(Gtk.Window):
             print('New Mountpoint: ',self.mountpoint())
             return True
 
-    def noobsInstaller(self):
-        print('noobs installer')
-        flag = subprocess.call('wget https://downloads.raspberrypi.org/NOOBS_lite_latest -O /tmp/noobs.zip -s'.split())
-        if flag:
-            self.msg('Error in Downloading Noobs Installer')
-            # return True
-        else:
-            self.msg('Noobs Installer successfully downloaded now preparing to install for Noobs in USB Drive')
-            if self.formatDrive():
-                flag = subprocess.getstatusoutput('sudo unzip -o /tmp/noobs.zip -d {}'.format(self.mountpoint()))
-                if flag[0]:
-                    self.msg('Error in installing Noobs')
-                else:
-                    self.msg('Noobs Successfully Installed in {}'.format(self.mountpoint()), flag[1])
-            else:
-                print('Error in formating Drive')
-                # return True        
-
-    def berrybootInstaller(self):
-        print('Berryboot installer')
-        flag = subprocess.call('wget -q http://downloads.sourceforge.net/project/berryboot/berryboot-20180405-pi0-pi1-pi2-pi3.zip -O /tmp/berryboot.zip'.split())
-        if flag[0]:
-            self.msg('Error in Downloading BerryBoot Installer')
-            # return True
-        else:
-            self.msg('Berry Installer successfully downloaded now preparing to install for Noobs in USB Drive')
-            if self.formatDrive():
-                flag = subprocess.getstatusoutput('sudo unzip -o /tmp/berryboot.zip -d {}'.format(self.mountpoint()))
-                if flag[0]:
-                    self.msg('Error in installing Berryboot')
-                    print(flag[1])
-                else:
-                    self.msg('Berryboot Successfully Installed in {}'.format(self.mountpoint()), flag[1])
-            else:
-                print('Error in formating Drive')
-                # return True 
-        
-
-    def formatButtonClicked(self, widget):
-        self.loading.pulse()
-        self.formatDrive()
-
-    def flashButtonClicked(self, widget):
-        if self.choice == 'selectImage':
-            print('Image')
-        elif self.choice == 'selectOS':
-            print('SelectOS')
-        elif self.choice == 'selectBootloader':
-            if self.bootloaderChoice == 'noobs':
-                self.noobsInstaller()
-            elif self.bootloaderChoice == 'pinn':
-                print('Pinn')
-            elif self.bootloaderChoice == 'berryboot':
-                self.berrybootInstaller()
-            else:
-                self.msg('Please select Bootloader')
-        else:
-            self.msg('Please Choose Method')
-
-
-    def pinnInstaller(self):
+    def pinnInstaller(self):                                # DOWNLOAD AND INSTALL PINN BOOTLOADER IN USB DRIVE OR SD CARD AUTOMATICALLY
         print('PINN installer')
+        self.lmsg('Wait for 5 mins and make some coffee')
         flag = subprocess.call('wget https://sourceforge.net/projects/pinn/files/latest/download -O /tmp/pinn.zip -s'.split())
         if flag:
             self.msg('Error in Downloading PINN Installer')
@@ -241,34 +203,86 @@ class FlashyPi(Gtk.Window):
             else:
                 print('Error in formating Drive')
                 # return True 
+
+    def noobsInstaller(self):                           # DOWNLOAD AND INSTALL NOOBS BOOTLOADER IN USB DRIVE OR SD CARD AUTOMATICALLY
+        print('noobs installer')
+        flag = subprocess.call('wget https://downloads.raspberrypi.org/NOOBS_lite_latest -O /tmp/noobs.zip'.split())
+        if flag:
+            self.msg('Error in Downloading Noobs Installer')
+            # return True
+        else:
+            self.msg('Noobs Installer successfully downloaded now preparing to install for Noobs in USB Drive')
+            if self.formatDrive():
+                flag = subprocess.getstatusoutput('sudo unzip -o /tmp/noobs.zip -d {}'.format(self.mountpoint()))
+                if flag[0]:
+                    self.msg('Error in installing Noobs')
+                else:
+                    self.msg('Noobs Successfully Installed in {}'.format(self.mountpoint()), flag[1])
+            else:
+                print('Error in formating Drive')
+                # return True        
+
+    def berrybootInstaller(self):                       # DOWNLOAD AND INSTALL BERRYBOOT BOOTLOADER IN USB DRIVE AND SD CARD AUTOMATICALLY
+        self.lmsg('Wait for 5 mins and make some coffee')
+        flag = subprocess.getstatusoutput('wget -q http://downloads.sourceforge.net/project/berryboot/berryboot-20180405-pi0-pi1-pi2-pi3.zip -O /tmp/berryboot.zip')
+        if flag[0]:
+            self.msg('Error in Downloading BerryBoot Installer',flag[1])
+            self.lmsg('Error in Downloading BerryBoot Installer')
+            # return True
+        else:
+            self.msg('Berry Installer successfully downloaded now preparing to install for Noobs in USB Drive')
+            self.lmsg('Berry Installer successfully downloaded now preparing to install for Noobs in USB Drive')
+            if self.formatDrive():
+                flag = subprocess.getstatusoutput('sudo unzip -o /tmp/berryboot.zip -d {}'.format(self.mountpoint()))
+                if flag[0]:
+                    self.msg('Error in installing Berryboot')
+                    print(flag[1])
+                else:
+                    self.msg('Berryboot Successfully Installed in {}'.format(self.mountpoint()), flag[1])
+            else:
+                print('Error in formating Drive')
+                # return True 
         
-
-    def otherButtonClicked(self, widget,):
-        # self.textBuffer.insert(self.textBuffer.get_end_iter(), "AS saod D", -1)
+    # THIS FUCTION CALL WHEN FORMAT BUTTON CLICKED
+    def formatButtonClicked(self, widget):
         self.loading.pulse()
-        print(self.mountpoint())
+        self.formatDrive()
 
+    # THIS FUNCTION CALL WHEN FLASH BUTTON CLICKED
+    def flashButtonClicked(self, widget):
+        if self.choice == 'selectImage':        # CHECK RADIO BUTTON THEN ACT ACCORDINGLY
+            print('Image')
+        elif self.choice == 'selectOS':
+            print('SelectOS')
+        elif self.choice == 'selectBootloader':
+            if self.bootloaderChoice == 'noobs':
+                self.noobsInstaller()
+            elif self.bootloaderChoice == 'pinn':
+                print('Pinn')
+            elif self.bootloaderChoice == 'berryboot':
+                self.berrybootInstaller()
+            else:
+                self.msg('Please select Bootloader')
+        else:
+            self.msg('Please Choose Method')
+       
+
+    # MESSAGES PASSING IN LOG ENGINE
     def msg(self, *text):
         # print(text)
         self.textBuffer.insert(self.textBuffer.get_start_iter(), str(text)+'\n', -1)
+    
+    # MESSAGES PASSING ON LOADING BAR
+    def lmsg(self, text):
+        print('loading text ',text)
+        self.loading.pulse()
+        self.loading.set_text(text)
+        self.loading.set_show_text(True)
 
-    def run(self):
+    def run(self):                      # THIS FUNCTION IS CALLED WHEN YOU RUN THIS PROGRAM
         self.mainWin.show_all()
         Gtk.main()
         pass
 
-
-# if len(argv)>0:
-#     argv = argv[1]
-# else:
-#     agrv = None
-
-# print(len(argv))
 win = FlashyPi()
-win.run()
-
-
-
-#sudo parted /dev/{} mklabel msdos mkpart primary fat32 1MiB 100% set 1 boot on
-
-# sudo mkfs.vfat /dev/sdb1
+win.run()           # RUN THE ENGINE
